964d9129f1a22535b4e9cf8da14565975174a64c
Taha Tuncay <110984520+tahatuncay@users.noreply.github.com>
2023-07-08 18:29:24 +0300

    first day

---
+++ day1exercises.js
@@ -0,0 +1,42 @@
+// Variables
+
+let firstName = "Taha";
+console.log(firstName);
+
+let isStudent = true;
+console.log(isStudent);
+
+let age = 21;
+console.log(age);
+
+let studentID;
+console.log(studentID);
+
+let statuss = null;
+console.log(statuss);
+
+// Data Types
+
+console.log(typeof(firstName));
+
+console.log(typeof(age));
+
+console.log(typeof(isStudent));
+
+console.log(typeof(studentID));
+
+console.log(typeof(statuss));
+
+// -----------------------------------
+
+let n1,n2,n3,n4;
+n2 = 2;
+console.log(n2);
+
+//-------------------------------------
+
+let myAge =21;
+    yourAge = 25;
+
+console.log("My age is "+myAge +", and your age is "+yourAge);
+
